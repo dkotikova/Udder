f9dcead034cb83ccf860179299a5a5806874a4ff
Daria Kotikova <daria_kotikova@icloud.com>
2025-02-01 21:06:09 +0300

    Initial Commit

---
+++ UdderTests/UdderTests.swift
@@ -0,0 +1,17 @@
+//
+//  UdderTests.swift
+//  UdderTests
+//
+//  Created by Дарья on 01.02.2025.
+//
+
+import Testing
+@testable import Udder
+
+struct UdderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
